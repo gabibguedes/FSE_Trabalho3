a8c0529ffe3be3d266f563cd19625eecb95d1a17
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-04-08 18:53:19 -0300

    Add next js default styles files
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- central_server/pages/index.js
+++ central_server/pages/index.js
@@ -1,60 +1,19 @@
-import { useState, useRef } from 'react'
-import useMqtt from '../hooks/useMqtt'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Mqtt from '../components/mqtt'
 
 export default function Home() {
-  const [incommingMessages, setIncommingMessages] = useState([])
-  const addMessage = (message) => {
-    setIncommingMessages((incommingMessages) => [...incommingMessages, message])
-  }
-  const clearMessages = () => {
-    setIncommingMessages(() => [])
-  }
-
-  const incommingMessageHandlers = useRef([
-    {
-      topic: 'topic1',
-      handler: (msg) => {
-        addMessage(msg)
-      },
-    },
-  ])
-
-  const mqttClientRef = useRef(null)
-  const setMqttClient = (client) => {
-    mqttClientRef.current = client
-  }
-  useMqtt({
-    uri: process.env.NEXT_PUBLIC_MQTT_URI,
-    options: {
-      clientId: process.env.NEXT_PUBLIC_MQTT_CLIENTID,
-    },
-    topicHandlers: incommingMessageHandlers.current,
-    onConnectedHandler: (client) => setMqttClient(client),
-  })
-
-  const publishMessages = (client) => {
-    if (!client) {
-      console.log('(publishMessages) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('topic1', '1st message from component')
-  }
-
   return (
-    <div>
-      <h2>Subscribed Topics</h2>
-      {incommingMessageHandlers.current.map((i) => (
-        <p key={Math.random()}>{i.topic}</p>
-      ))}
-      <h2>Incomming Messages:</h2>
-      {incommingMessages.map((m) => (
-        <p key={Math.random()}>{m.payload.toString()}</p>
-      ))}
-      <button onClick={() => publishMessages(mqttClientRef.current)}>
-        Publish Test Messages
-      </button>
-      <button onClick={() => clearMessages()}>Clear Test Messages</button>
+    <div className={styles.container}>
+      <Head>
+        <title>Central server</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <Mqtt/>
+      </div>
     </div>
   )
 }
